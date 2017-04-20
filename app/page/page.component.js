994b760cbb305a823ce3d80dd9ecc165b87f8720
manudevelopia <manugarciarodriguez@gmail.com>
2017-04-20 16:24:54 +0200

    Secctions are rendered

--- app/page/page.component.js
+++ app/page/page.component.js
@@ -16,8 +16,28 @@
     var vm = this;
 
     vm.$onInit = function() {
-
+      vm.page = {};
+      vm.page.sections = sections;
     };
 
   }
+
+  var sections = [
+    {
+      title: 'This is my title 1',
+      description: 'This is a custom description that can be used to do things',
+      picture: 'img/pexels-photo-70292.jpeg'
+    },
+    {
+      title: 'This is my title 2',
+      description: 'This is a custom description that can be used to do things',
+      picture: 'img/pexels-photo-57825.jpeg'
+    },
+    {
+      title: 'This is my title 3',
+      description: 'This is a custom description that can be used to do things',
+      picture: 'img/pexels-photo-196846.jpeg'
+    },
+  ]
+  ;
 })();
--- app/section-area/section.area.component.js
+++ app/section-area/section.area.component.js
@@ -15,14 +15,7 @@
   function SectionController(){
     var vm = this;
 
-    vm.$onInit = function() {
-      vm.section = section;
-    };
 
   }
 
-  var section = {};
-  section.title = 'This is my title';
-  section.description = 'This is a custom description that can be used to do things';
-  section.picture = 'img/pexels-photo-70292.jpeg';
 })();
